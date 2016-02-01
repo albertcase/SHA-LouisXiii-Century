fa0838c805cf6987cea130aa7628834c91769e55
Demon Zhang <demon.zhang@samesamechina.com>
2016-02-01 16:35:27 +0800

    no message

--- ticket.php
+++ ticket.php
@@ -1,14 +1,23 @@
 <?php
+ini_set("display_errors", 1);
 $access_token = file_get_contents("http://wx.bysoftchina.com.cn/wx-token/token.php");
+echo $access_token;
+echo "</br>1</br>";
 $ticketfile = file_get_contents("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=".$access_token."&type=jsapi");
+echo $ticketfile;
+echo "</br>2</br>";
 $ticketfile = json_decode($ticketfile, true);
 $ticket = $ticketfile['ticket'];
 $time = time()+ $ticketfile['expires_in'];
+echo $time;
+echo "</br>3</br>";
+echo $ticket;
+echo "</br>4</br>";
 $fp = fopen("time.txt", "w");
 fwrite($fp,$time);
 fclose($fp);
 $fp = fopen("ticket.txt", "w");
 fwrite($fp,$ticket);
 fclose($fp);
-echo $ticket;	
+echo 1;	
 ?>
